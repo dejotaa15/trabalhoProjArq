83f51933cea4088cf854eaa0cf9adb0ed03cdb1c
dejotaa15 <joao.bianchi06@gmail.com>
2026-08-19 20:22:35 -0300

    atualizacao

--- ex0-desafioPadroesDeProjeto/Q4_Visualizadores/src/FonteDeDados.java
+++ ex0-desafioPadroesDeProjeto/Q4_Visualizadores/src/FonteDeDados.java
@@ -4,14 +4,26 @@ import java.util.List;
 
 public class FonteDeDados{
     private List<Integer> lst;
+    private List<Observador> observadores = new ArrayList<>();
+
 
     public FonteDeDados(){
         lst = new LinkedList<>();
     }
+    public void adicionaObservador(Observador o){
++       observadores.add(o);
++   }
++
++   public void removeObservador(Observador o){
++       observadores.remove(o);
++   }
 
     public void add(Integer value){
         if (value < 0) throw new IllegalArgumentException("Valor invalido");
         lst.add(value);
+        for (Observador o : observadores){
++           o.notifica(value);
++       }
     }
 
     public int quantidade(){
--- ex0-desafioPadroesDeProjeto/Q4_Visualizadores/src/VisualizadorDeMedia.java
+++ ex0-desafioPadroesDeProjeto/Q4_Visualizadores/src/VisualizadorDeMedia.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class VisualizadorDeMedia {
+public class VisualizadorDeMedia implements Observador {
     private List<Integer> valores;
 
     public VisualizadorDeMedia(List<Integer> valores){
@@ -22,4 +22,11 @@ public class VisualizadorDeMedia {
             .orElse(0.0);
         System.out.println("Media: "+media+", quantidade de elementos analisados: "+valores.size());
     }
+    
+    @Override
++     public void notifica(Integer valor){
++         acrescentaValor(valor);
++         exibeMedia();
++     }
+
 }
